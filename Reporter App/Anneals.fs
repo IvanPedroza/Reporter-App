c41c7a8a1dc254c83ef0fe34afb09fb5b5ba148f
IvanPedroza <41658185+IvanPedroza@users.noreply.github.com>
2023-10-19 13:56:47 -0700

    Update Anneals.fs

--- Reporter App/Anneals.fs
+++ Reporter App/Anneals.fs
@@ -10,24 +10,22 @@ open System.Linq
 open System.Diagnostics
 open AnnealHelpers
 
+// Function used to fill out annealing Batch Records
 let annealStart (inputParams : string list) (annealsForm : string) (reporter : ExcelWorksheet) (myTools : ExcelWorksheet) =
-   
+    // Gets logged user for use in path and error logging
     let user = Environment.UserName
-
+    // User interface
     Console.WriteLine "Which bench space are you using?"
     let benchInput = Console.ReadLine ()
-
-    //Reading in reagents excel book
-    //Console.WriteLine "Which reagents are you using?"
-    let reagentsInput = "anneals" //Console.ReadLine ()
-
-    let heatBlockInput = "heatblocks" //Console.ReadLine ()
+    let reagentsInput = "anneals"
+    let heatBlockInput = "heatblocks"
 
 
 
-    //Starting point for manipulating sheet info and storing it in "param"
+    // Starting point for manipulating sheet info and storing it in "param"
     for param in inputParams do
 
+        // Reads in equipments IDs for bench specified from LIMS
         let pipetteCalibration = listFunction benchInput myTools 2
         let p1000 = listFunction benchInput myTools 3
         let p1000Id = listFunction benchInput myTools 4
@@ -48,7 +46,7 @@ let annealStart (inputParams : string list) (annealsForm : string) (reporter : E
         let mc8P200Id = listFunction benchInput myTools 20
         let mc12P200Id = listFunction benchInput myTools 24
 
-
+        // Reads in reagent lot info from LIMS
         let trisLot = listFunction reagentsInput myTools 2
         let trisExp = listFunction reagentsInput myTools 3
         let trisUbd = listFunction reagentsInput myTools 4
@@ -62,7 +60,6 @@ let annealStart (inputParams : string list) (annealsForm : string) (reporter : E
         let ethanolUbd = listFunction reagentsInput myTools 12
         let dv1Lot = listFunction reagentsInput myTools 13
         let dv1Exp = listFunction reagentsInput myTools 14
-
         let h37Id = listFunction heatBlockInput myTools 2
         let h37Cal = listFunction heatBlockInput myTools 3
         let h45Id = listFunction heatBlockInput myTools 4
@@ -73,25 +70,24 @@ let annealStart (inputParams : string list) (annealsForm : string) (reporter : E
         let h75Cal = listFunction heatBlockInput myTools 9
 
       
-        
+        // Reads in blank Batch Record template 
         let readExcelBytes = File.ReadAllBytes(annealsForm)
         let excelStream = new MemoryStream(readExcelBytes)
         let formPackage = new ExcelPackage(excelStream)
         let annealSheet = formPackage.Workbook.Worksheets.["FRM-M0055"]
         
 
-        //Takes value of each cell of the row in which the input lies and stores it for use in filling out Word Doc
+        // Takes value of each cell of the row in which the input lies and stores it for use in filling out Word Doc
         let lot = listFunction param reporter 1 
         let csName = listFunction param reporter 2 
         let geneNumber = listFunction param reporter 5  |> float
         let scale = listFunction param reporter 6  |> float
            
 
-
+        // Fills out Batch Record at specified cells
         annealSheet.Cells.[5, 2].Value <- lot + " " + csName
         annealSheet.Cells.[5, 5].Value <- geneNumber
         annealSheet.Cells.[5, 7].Value <- scale
-
         annealSheet.Cells.[8, 4].Value <- trisLot
         annealSheet.Cells.[8, 6].Value <- trisExp
         annealSheet.Cells.[9, 6].Value <- "Use by date: " + trisUbd
@@ -127,16 +123,6 @@ let annealStart (inputParams : string list) (annealsForm : string) (reporter : E
         annealSheet.Cells.[27, 6].Value <- pipetteCalibration
 
 
+        // Saves filled out Batch Record for printing and deletion
         let user = Environment.UserName
         let path = "C:/Users/" + user + "//AppData/Local/Temp/ "+param + " RP Anneal Batch Record" + ".xlsx"
-
-         
-        
-        let tests = formPackage.GetAsByteArray()
-
-        File.WriteAllBytes(path, tests)
-
-        
-
-
-
